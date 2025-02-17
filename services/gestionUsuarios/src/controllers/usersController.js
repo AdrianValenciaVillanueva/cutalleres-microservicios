a37c3a260eaf4b1e5199f3578b7df3d5e71de012
Adrian Valencia <villanuevaadrian123@gmail.com>
2025-02-17 16:26:53 -0600

    incriptacion de contrasena con salt de 10 rondas

--- services/gestionUsuarios/src/controllers/usersController.js
+++ services/gestionUsuarios/src/controllers/usersController.js
@@ -1,11 +1,13 @@
 const db = require('../../models');
+const bcrypt = require('bcrypt');
 
 const createUser = async (req, res) => {
     try{
+        const hashedPassword = await bcrypt.hash(req.body.contrasena, 10);
         const usuario = await db.Usuario.create({
             codigo_udg: req.body.codigo_udg,
             correo: req.body.correo,
-            contrasena: req.body.contrasena
+            contrasena: hashedPassword
         });
         res.status(201).json(usuario);
     }catch(error){
@@ -14,7 +16,12 @@ const createUser = async (req, res) => {
 };
 
 const getUser = async (req, res) => {
-    res.send("Usuario obtenido");
+    try{
+        const usuarios = await db.Usuario.findAll();
+        res.status(200).json(usuarios);
+    }catch(error){
+        res.status(500).json({error: "Error al obtener usuario"});
+    }
 }
 
 const updateUser = async (req, res) => {
